bd856ab36c27b1ff0cf05cacefea94d4f9b2ed2a
gchance22 <graham@doublebluesports.com>
2016-10-17 19:23:25 -0400

    used ibm fork of swiftyjson
    Fixes compile issue in ubuntu

--- Package.swift
+++ Package.swift
@@ -5,7 +5,7 @@ let package = Package(
     dependencies: [
         .Package(url: "https://github.com/PerfectlySoft/Perfect-CURL.git", majorVersion: 2, minor: 0),
         .Package(url: "https://github.com/PerfectlySoft/Perfect-libcurl.git", majorVersion: 2, minor: 0),
-        .Package(url: "https://github.com/SwiftyJSON/SwiftyJSON.git", majorVersion: 3, minor: 1),
+        .Package(url: "https://github.com/IBM-Swift/SwiftyJSON.git", majorVersion: 14, minor: 2),
     ]
 
 )
