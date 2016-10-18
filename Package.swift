f537772ea31576892e962cebfbd971a0fd357dc1
gchance22 <graham@doublebluesports.com>
2016-10-18 18:59:54 -0400

    removed unused dependencies

--- Package.swift
+++ Package.swift
@@ -3,8 +3,6 @@ import PackageDescription
 let package = Package(
     name: "FirebaseSwift",
     dependencies: [
-        .Package(url: "https://github.com/IBM-Swift/SwiftyJSON.git", majorVersion: 14, minor: 2),
-        .Package(url: "https://github.com/daltoniam/SwiftHTTP.git", majorVersion: 2, minor: 0),
         .Package(url: "https://github.com/JustHTTP/Just.git", majorVersion: 0, minor: 5)
     ]
 
