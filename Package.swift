f2a2b2f6db79880f102a60d8129b46f36148c364
gchance22 <graham@doublebluesports.com>
2016-10-18 18:52:17 -0400

    switched to JustHTTP/Just for http

--- Package.swift
+++ Package.swift
@@ -3,9 +3,9 @@ import PackageDescription
 let package = Package(
     name: "FirebaseSwift",
     dependencies: [
-        .Package(url: "https://github.com/PerfectlySoft/Perfect-CURL.git", majorVersion: 2, minor: 0),
-        .Package(url: "https://github.com/PerfectlySoft/Perfect-libcurl.git", majorVersion: 2, minor: 0),
         .Package(url: "https://github.com/IBM-Swift/SwiftyJSON.git", majorVersion: 14, minor: 2),
+        .Package(url: "https://github.com/daltoniam/SwiftHTTP.git", majorVersion: 2, minor: 0),
+        .Package(url: "https://github.com/JustHTTP/Just.git", majorVersion: 0, minor: 5)
     ]
 
 )
--- Source/Firebase.swift
+++ Source/Firebase.swift
@@ -7,11 +7,11 @@
 //
 
 import Foundation
-import PerfectCURL
-import cURL
 import SwiftyJSON
+import Just
 
-private enum CurlMethod: String {
+
+private enum Method: String {
     case delete = "DELETE"
     case post = "POST"
     case get = "GET"
@@ -25,7 +25,7 @@ public class Firebase {
 
     public let baseURL: String
 
-    public let timeout = 30 // seconds
+    public let timeout: Double = 30.0 // seconds
 
     public init(baseURL: String = "", auth: String? = nil) {
         self.auth = auth
@@ -39,48 +39,56 @@ public class Firebase {
     }
 
 
-    public func post(path: String, value: Any)->[String: AnyObject]? {
+    public func setValue(path: String, value: Any)->[String: Any]? {
+       return put(path: path, value:  value)
+    }
+
+
+    public func post(path: String, value: Any)->[String: Any]? {
         return write(value: value, path: path, method: .post)
     }
 
-    public func put(path: String, value: Any)->[String: AnyObject]? {
+
+    public func put(path: String, value: Any)->[String: Any]? {
         return write(value: value, path: path, method: .put)
     }
 
-    public func patch(path: String, value: Any)->[String: AnyObject]? {
+    public func patch(path: String, value: Any)->[String: Any]? {
         return write(value: value, path: path, method: .patch)
     }
 
+
     public func delete(path: String) {
         let url = completeURLWithPath(path: path)
-        let curl = createCurl(url: url, type: .delete)
-        let response = curl.performFully()
-        print(response.0)
+        let result = Just.delete(url)
+        if let error = result.error {
+            print("DELETE Error: " + error.localizedDescription)
+        }
     }
 
 
-    public func get(path: String) ->AnyObject? {
+    public func get(path: String) ->Any? {
 
         let url = completeURLWithPath(path: path)
-        let curl = createCurl(url: url, type: .get)
+        let httpResult = Just.get(url)
 
-        let response = curl.performFully()
-        let data = Data(response.2)
-
-        curl.close()
-
-        do {
-            if let jsonMap = try JSONSerialization.jsonObject(with: data, options: [JSONSerialization.ReadingOptions.allowFragments]) as AnyObject? {
-                return jsonMap
+        if let data = httpResult.content, httpResult.error == nil {
+            do {
+                if let jsonMap = try JSONSerialization.jsonObject(with: data, options: [JSONSerialization.ReadingOptions.allowFragments]) as Any? {
+                    return jsonMap
+                }
+            } catch let e {
+                print("GET Error:" + e.localizedDescription)
             }
-        } catch let e {
-            print(e.localizedDescription)
+        } else {
+            print("GET request failed.")
+            if let error = httpResult.error {
+                print(error)
+            }
         }
+
         return nil
-
     }
-
-
 
 
     private func completeURLWithPath(path: String)->String {
@@ -92,50 +100,44 @@ public class Firebase {
     }
 
 
-    private func createCurl(url: String, type: CurlMethod)->CURL {
-        let curl = CURL(url: url)
-        let _ = curl.setOption(CURLOPT_CUSTOMREQUEST, s: type.rawValue)
-        let _ = curl.setOption(CURLOPT_TIMEOUT, int: timeout)
-        let _ = curl.setOption(CURLOPT_CONNECTTIMEOUT, int: timeout)
-        let _ = curl.setOption(CURLOPT_HTTPHEADER, s: "Accept: application/json")
-        return curl
-    }
+    private func write(value: Any, path: String, method: Method)->[String: Any]? {
 
+        let url = completeURLWithPath(path: path)
+        let params = [String: Any]()
+        let data = [String: Any]()
+        let json: Any? = JSONSerialization.isValidJSONObject(value) ? value : [".value": value]
+        let headers = ["Accept": "application/json"]
+        let files = [String: HTTPFile]()
+        let auth: (String, String)? = nil
+        let cookies = [String: String]()
+        let allowRedirects = false
+        let requestBody: Data? = nil
+        let urlQuery: String? = nil
+        let completion: ((HTTPResult)->())? = nil
+        let progress: (TaskProgressHandler)? = nil
 
-    private func write(value: Any, path: String, method: CurlMethod)->[String: AnyObject]? {
+        let result: HTTPResult?
+        switch method {
+        case .put:
+            result = Just.put(url, params: params, data: data, json: json, headers: headers, files: files, auth: auth, cookies: cookies, allowRedirects: allowRedirects, timeout: timeout, requestBody: requestBody, URLQuery: urlQuery, asyncProgressHandler: progress, asyncCompletionHandler: completion)
+        case .post:
+            result = Just.post(url, params: params, data: data, json: json, headers: headers, files: files, auth: auth, cookies: cookies, allowRedirects: allowRedirects, timeout: timeout, requestBody: requestBody, URLQuery: urlQuery, asyncProgressHandler: progress, asyncCompletionHandler: completion)
+        case .patch:
+            result = Just.patch(url, params: params, data: data, json: json, headers: headers, files: files, auth: auth, cookies: cookies, allowRedirects: allowRedirects, timeout: timeout, requestBody: requestBody, URLQuery: urlQuery, asyncProgressHandler: progress, asyncCompletionHandler: completion)
+        default:
+            result = nil
+        }
+
         do {
-            let bytes = try createBytes(value: value)
-            let url = completeURLWithPath(path: path)
-
-            let curl = createCurl(url: url, type: method)
-            let _ = curl.setOption(CURLOPT_POSTFIELDS, v: UnsafeMutableRawPointer(mutating: bytes))
-            let _ = curl.setOption(CURLOPT_POSTFIELDSIZE, int: bytes.count)
-
-            let response = curl.performFully()
-            let data = Data(response.2)
-            curl.close()
-
-            if let jsonMap = try JSONSerialization.jsonObject(with: data, options: []) as? [String: AnyObject] {
+            if let data = result?.content,
+                let jsonMap = try JSONSerialization.jsonObject(with: data, options: [JSONSerialization.ReadingOptions.allowFragments]) as? [String: Any] {
                 return jsonMap
-            } else {
-                print("Firebase response not a valid UTF-8 sequence")
             }
-
         } catch let e {
-            print(e.localizedDescription)
+            print(method.rawValue + " Error: " + e.localizedDescription)
         }
 
         return nil
-    }
-
-
-    private func createBytes(value: Any) throws ->[UInt8] {
-        let json = JSON(value)
-        let data = try json.rawData()
-        let array = data.withUnsafeBytes {
-            [UInt8](UnsafeBufferPointer(start: $0, count: data.count))
-        }
-        return array
     }
 
 }
