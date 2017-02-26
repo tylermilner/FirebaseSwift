f6e2645781a10941fba3e466e8cc41ab57e80e23
Graham Chance <graham@gtcsoftware.com>
2017-02-26 13:24:35 +0100

    Documentation

--- Source/Firebase.swift
+++ Source/Firebase.swift
@@ -20,13 +20,25 @@ private enum Method: String {
 
 public class Firebase {
 
+    
+    /// Auth token
     public var auth: String?
+    
+    /// Base URL (e.g. http://myapp.firebaseio.com)
     public let baseURL: String
+    
+    /// Timeout of http operations
     public let timeout: Double = 30.0 // seconds
 
     private let headers = ["Accept": "application/json"]
 
 
+    
+    /// Constructor
+    ///
+    /// - Parameters:
+    ///   - baseURL: Base URL (e.g. http://myapp.firebaseio.com)
+    ///   - auth: Auth token
     public init(baseURL: String = "", auth: String? = nil) {
         self.auth = auth
 
@@ -38,51 +50,128 @@ public class Firebase {
 
     }
 
-
+    
+    /// Performs a synchronous PUT at given path from the base url
+    ///
+    /// - Parameters:
+    ///   - path: path to append to base url
+    ///   - value: value to PUT
+    /// - Returns: resulting data
     public func setValue(path: String, value: Any)->[String: AnyObject]? {
         return put(path: path, value:  value)
     }
 
+    
+    /// Performs an asynchronous PUT at base url plus given path
+    ///
+    /// - Parameters:
+    ///   - path: path to append to base url
+    ///   - value: value to PUT
+    ///   - complete: callback containing resulting data as a parameter
     public func setValueAsync(path: String, value: Any, complete: @escaping ([String: AnyObject]?)->()) {
         putAsync(path: path, value:  value, complete: complete)
     }
 
+    
+    /// Performs a synchronous POST at given path from the base url
+    ///
+    /// - Parameters:
+    ///   - path: path to append to the base url
+    ///   - value: value to post
+    /// - Returns: resulting data
     public func post(path: String, value: Any)->[String: AnyObject]? {
         return write(value: value, path: path, method: .post, complete: nil)
     }
 
+    
+    /// Performs an asynchronous POST at given path from the base url
+    ///
+    /// - Parameters:
+    ///   - path: path to append to the base url
+    ///   - value: value to post
+    ///   - complete: callback containing resulting data as a parameter
     public func postAsync(path: String, value: Any, complete: @escaping ([String: AnyObject]?)->()) {
         let _ = write(value: value, path: path, method: .post, complete: complete)
     }
 
+    
+    /// Performs a synchronous PUT at given path from the base url
+    ///
+    /// - Parameters:
+    ///   - path: path to append to the base url
+    ///   - value: value to put
+    /// - Returns: resulting data
     public func put(path: String, value: Any)->[String: AnyObject]? {
         return write(value: value, path: path, method: .put, complete: nil)
     }
 
+    
+    /// Performs an asynchronous PUT at given path from the base url
+    ///
+    /// - Parameters:
+    ///   - path: path to append to the base url
+    ///   - value: value to put
+    ///   - complete: callback containing resulting data as a parameter
     public func putAsync(path: String, value: Any, complete: @escaping ([String: AnyObject]?)->()) {
         let _ = write(value: value, path: path, method: .put, complete: complete)
     }
 
+    
+    /// Performs a synchronous PATCH at given path from the base url
+    ///
+    /// - Parameters:
+    ///   - path: path to append to the base url
+    ///   - value: value to put
+    /// - Returns: resulting data
     public func patch(path: String, value: Any)->[String: AnyObject]? {
         return write(value: value, path: path, method: .patch, complete: nil)
     }
 
+    
+    /// Performs an asynchronous PATCH at given path from the base url
+    ///
+    /// - Parameters:
+    ///   - path: path to append to the base url
+    ///   - value: value to put
+    ///   - complete: callback containing resulting data as a parameter
     public func patchAsync(path: String, value: Any, complete: @escaping ([String: AnyObject]?)->()) {
         let _ = write(value: value, path: path, method: .patch, complete: complete)
     }
 
+    
+    /// Performs a synchronous DELETE at given path from the base url
+    ///
+    /// - Parameter path: path to append to the base url
+    /// - Returns: deleted data
     public func delete(path: String)->[String: AnyObject]? {
         return delete(path: path, complete: nil)
     }
 
+    
+    /// Performs an asynchronous DELETE at given path from the base url
+    ///
+    /// - Parameters:
+    ///   - path: <#path description#>
+    ///   - complete: callback containing deleted data as a parameter
     public func deleteAsync(path: String, complete: @escaping ([String: AnyObject]?)->()) {
         let _ = delete(path: path, complete: complete)
     }
 
+    
+    /// Performs a synchronous GET at given path from the base url
+    ///
+    /// - Parameter path: path to append to the base url
+    /// - Returns: data at the path location
     public func get(path: String)->Any? {
         return get(path: path, complete: nil)
     }
 
+    
+    /// Performs an asynchronous GET at given path from the base url
+    ///
+    /// - Parameters:
+    ///   - path: path to append to the base url
+    ///   - complete: callback containing the resulting data as a parameter
     public func getAsync(path: String, complete: @escaping (Any?)->()) {
         let _ = get(path: path, complete: complete)
     }
