c5adf87dec7966dc3dbb1433e122fd4cf3bd2d02
Graham Chance <graham@gtcsoftware.com>
2017-07-14 18:35:17 -0400

    Updated to Swift 3.1

--- Package.swift
+++ Package.swift
@@ -3,7 +3,7 @@ import PackageDescription
 let package = Package(
     name: "FirebaseSwift",
     dependencies: [
-        .Package(url: "https://github.com/gtchance/Just.git", majorVersion: 0, minor: 5)
+        .Package(url: "https://github.com/gtchance/Just.git", majorVersion: 0, minor: 6)
     ]
 
 )
