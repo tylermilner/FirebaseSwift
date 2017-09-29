b7a1525289b2b0c669d48e7e869fb6fcaedc30f0
Graham Chance <graham@gtcsoftware.com>
2017-09-29 14:06:16 -0400

    Use linux compatible swiftyjson

--- Package.swift
+++ Package.swift
@@ -4,7 +4,7 @@ let package = Package(
     name: "FirebaseSwift",
     dependencies: [
         .Package(url: "https://github.com/gtchance/Just.git", majorVersion: 0, minor: 6),
-        .Package(url: "https://github.com/SwiftyJSON/SwiftyJSON.git", majorVersion: 3, minor: 1),
+        .Package(url: "https://github.com/IBM-Swift/SwiftyJSON.git", majorVersion: 16, minor: 0),
     ]
 
 )
