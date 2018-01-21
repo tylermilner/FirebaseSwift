ae7fc6a88c15ae79fb3ba4d1f2f261ed51f08fc0
Tyler Milner <tylermilner@gmail.com>
2018-01-21 14:20:23 -0800

    Reworked Package.swift file for Swift 4.

--- Package.swift
+++ Package.swift
@@ -1,9 +1,18 @@
+// swift-tools-version:4.0
+
 import PackageDescription
 
 let package = Package(
     name: "FirebaseSwift",
+    products: [
+        .library(name: "FirebaseSwift", targets: ["FirebaseSwift"]),
+    ],
     dependencies: [
-        .Package(url: "https://github.com/gtchance/Just.git", majorVersion: 0, minor: 6)
+        .package(url: "https://github.com/tylermilner/Just.git", .upToNextMajor(from: "0.6.1"))
+    ],
+    targets: [
+        .target(name: "FirebaseSwift", dependencies: ["Just"],
+                exclude: []),
+        .testTarget(name: "FirebaseSwiftTests", dependencies: ["FirebaseSwift"])
     ]
-
 )
